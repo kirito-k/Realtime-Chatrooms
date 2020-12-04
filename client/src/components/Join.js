dd02a8fc72bedd9b6fa82c68fac80536944a819f
kirito-k <devavratk96@gmail.com>
2020-12-04 15:08:35 -0500

    chat app project strucutre

---
+++ client/src/components/Join.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Join() {
+  return <h1>This is Join route</h1>;
+}
---
+++ server/index.js
@@ -0,0 +1,9 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket-io')(server);
+
+
+
+
+const port = process.env.PORT || 4000;
+server.listen(port, () => console.log(`Server runing on port ${port}`))
